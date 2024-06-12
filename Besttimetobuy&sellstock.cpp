ded1494650ef88ea08df870a0fec8e1f8e31dbbe
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-06-12 10:34:10 +0530

    Best Time To Buy & Sell Stock

---
+++ Besttimetobuy&sellstock.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buyPrice = prices[0];
+        int profit = 0;
+        for(int i = 1; i < prices.size(); i++){
+            if(buyPrice > prices[i]){
+                buyPrice = prices[i];
+            }
+            profit = max(profit, prices[i] - buyPrice);
+        } 
+        return profit;
+    }
+};
