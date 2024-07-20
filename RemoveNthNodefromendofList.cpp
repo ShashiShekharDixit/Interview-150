7cb7de870998408182497d1e2e7b2e39c921ddd4
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-07-20 10:28:52 +0530

    Remove Nth Node from End of List

---
+++ RemoveNthNodefromendofList.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        if(head == NULL || n <= 0) {
+            return head;
+        }
+        int count = 0;
+        ListNode* temp = head;
+        while(temp != NULL) {
+            count++;
+            temp = temp -> next;
+        }
+        int ans = count - n;
+        temp = head;
+        ListNode* prev = NULL;
+        int a = 0;
+        while(temp != NULL){
+            if(a == ans) {
+                if( prev != NULL){
+                    prev -> next = temp -> next;
+                } else {
+                    head = temp -> next;
+                }
+                delete temp;
+                return head;
+            }
+            prev = temp;
+            temp = temp -> next;
+            a++;
+        }
+        return head;
+    }
+};
