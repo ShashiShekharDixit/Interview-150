d30e90fae3875a106c6ff8395311c4eeb5724b4e
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-07-23 20:25:00 +0530

    Flatten Binary Tree From Linked List

---
+++ FlattenBinaryTreeFormLinkedList.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        TreeNode* curr = root;
+        while(curr) {
+            if(curr -> left) {
+                TreeNode* runner = curr -> left;
+                while(runner -> right) {
+                    runner = runner -> right;
+                }
+                runner -> right = curr -> right;
+                curr -> right = curr -> left;
+                curr -> left = nullptr;
+            }
+            curr = curr -> right;
+        }
+    }
+};
