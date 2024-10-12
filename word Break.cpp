5199a045e4e693fffd2b7dec3f71e4fb5f196dbd
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-10-12 20:23:13 +0530

    Word Break

--- word Break.cpp
+++ word Break.cpp
@@ -1,19 +1,31 @@
 class Solution {
-private:
-    bool wordBreak(string s, unordered_set<string> &set, int start){
-        if(start == s.size()){
-            return true;
+ public:
+  bool wordBreak(string s, vector<string>& wordDict) {
+    const int n = s.length();
+    const int maxLength = getMaxLength(wordDict);
+    const unordered_set<string> wordSet{begin(wordDict), end(wordDict)};
+    vector<int> dp(n + 1);
+    dp[0] = true;
+
+    for (int i = 1; i <= n; ++i)
+      for (int j = i - 1; j >= 0; --j) {
+        if (i - j > maxLength)
+          break;
+        if (dp[j] && wordSet.count(s.substr(j, i - j))) {
+          dp[i] = true;
+          break;
         }
-        for(int i = start; i < s.size();  i++){
-            if(set.count(s.substr(start, i + 1 - start)) &&wordBreak(s, set, i + 1)){
-                return true;
-            }
-        }
-        return false;
-    }
-public:
-    bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string> set(wordDict.begin(), wordDict.end());
-        return wordBreak(s, set, 0);
-    }
+      }
+
+    return dp[n];
+  }
+
+ private:
+  int getMaxLength(const vector<string>& wordDict) {
+    return max_element(begin(wordDict), end(wordDict),
+                       [](const auto& a, const auto& b) {
+                         return a.length() < b.length();
+                       })
+        ->length();
+  }
 };
